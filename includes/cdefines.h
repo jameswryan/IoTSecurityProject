4942d3e149db1dc2845241bc5f9c111a9249315d
James Ryan <james.w.ryan.tx@gmail.com>
2021-03-20 12:14:37 -0500

    Fixed typo

--- includes/cdefines.h
+++ includes/cdefines.h
@@ -9,7 +9,7 @@
 #define JOIN_MODE ABP		   // OTAA or ABP
 #define US915 8				   // See table in RAK811.h
 #define PACKAGE_TYPE_CONFIRM 1 // See table in RAK811.h
-#define LORA_CLASS_D 2		   // See table in RAK811.h
+#define LORA_CLASS_C 2		   // See table in RAK811.h
 #define RAK_WAKEUP 0
 #define RAK_SLEEP 1
 #define TXpin 11 // Set the virtual serial port pins
